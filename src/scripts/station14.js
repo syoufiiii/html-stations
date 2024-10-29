b2f818b0688edc7ce5f6c0970758d9be4408ba82
syoufiiii <tanabesyoukaic25.k13@icloud.com>
2024-10-29 17:08:06 +0900

    add:14

--- src/scripts/station14.js
+++ src/scripts/station14.js
@@ -1,22 +1,42 @@
 function getData() {
-    // これから作ろうとしているもので、JavaScriptでフルネームを生成する必要が出てきました。
-    // ただ現状持っている情報では、名前と苗字をそれぞれのみしかありません。
-    // そのため、JavaScriptで連結させて生成することにしました。
-    const test = [
-        { id: 1, first_name: '優', family_name: '大木', affiliation: 'TechTrain', is_student: false },
-        { id: 2, first_name: '太郎', family_name: '山田', affiliation: 'HogeHoge大学', is_student: true }
-    ];
-    return test.map(buildFullName)
+  const test = [
+    {
+      id: 1,
+      first_name: "優",
+      family_name: "大木",
+      affiliation: "TechTrain",
+      is_student: false,
+    },
+    {
+      id: 2,
+      first_name: "太郎",
+      family_name: "山田",
+      affiliation: "HogeHoge大学",
+      is_student: true,
+    },
+  ];
+
+  return test.map(buildFullName); // full_nameを追加したオブジェクトの配列を返す
 }
 
 function buildFullName(data) {
-    // Station14の問題はこちらです。想定する答えをここを書いてください。
-    // 氏名がわかるようにしつつ、半角スペースで繋いでください。
-    // またtest配列のそれぞれのオブジェクトに対して、full_nameのプロパティが追加されるように実装すること
+  return {
+    ...data,
+    full_name: `${data.family_name} ${data.first_name}`, // 苗字と名前の結合
+  };
 }
 
-// 出力が想定される答え
-// const test = [
-//     {id: 1, full_name: '大木 優', first_name: '優', family_name: '大木',affiliation: 'TechTrain', is_student: false},
-//     {id: 2, full_name: '山田 太郎', first_name: '太郎', family_name: '山田',affiliation: 'HogeHoge大学', is_student: true}
-// ];
+// HTMLにフルネームを表示する関数
+function displayFullNames() {
+  const data = getData(); // full_name を持つデータを取得
+  const resultList = document.getElementById("result"); // <ul>要素を取得
+
+  data.forEach((person) => {
+    const listItem = document.createElement("li"); // <li>を生成
+    listItem.innerText = person.full_name; // フルネームをセット
+    resultList.appendChild(listItem); // <ul>に追加
+  });
+}
+
+// ページがロードされたときに displayFullNames() を実行
+window.addEventListener("DOMContentLoaded", displayFullNames);
